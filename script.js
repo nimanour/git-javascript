8dd21bd897ace1323e0b6ea108fef28e2c73cfbc
Nima Nour <nour0043@algonquilive.com>
2020-01-13 15:36:59 -0500

    Participation 2

---
+++ script.js
@@ -0,0 +1,18 @@
+var name = 'Nima NOUR'
+
+console.log(`Hello, my name is ${name}`)
+
+var number = 25
+
+var product = number * 2
+
+console.log(`The number ${number} multiplied by 2 is ${number * 2}.`)
+
+console.log(`The number multiplied by 5 is ${product * 5}.`)
+
+console.log(`The result is divided by 5 is ${(product * 5) / number}`)
+
+let finalValue = (product * 5) / number
+
+console.log(`The result minus 7 is ${finalValue - 7}.`)
+
